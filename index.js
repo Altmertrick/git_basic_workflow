fa6d406a8e104e8331d80ef662f8005941d09336
Altmertrick <altmertricks@gmail.com>
2022-07-05 14:26:56 +0300

    1 added functions.js feature1

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log('git basic workflow');
+
+const isPal = (str) => {
+  let i = 0;
+  let j = str.length - 1;
+
+  while (i < j) {
+    if (str[i] === str[j]) {
+      i++;
+      j--;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
